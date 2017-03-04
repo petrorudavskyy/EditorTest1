3ebc21b00cbb46a72dfeffcc66ceee7dc2caa633
petrorudavskyy <petruk220896@gmail.com>
2017-03-04 22:19:22 +0200

    Add files

---
+++ index.js
@@ -0,0 +1,47 @@
+
+$(document).ready(function() {
+
+	var originalImageSrc = $('#editable-image').attr('src');
+	var currentImage; // assigned when the Edit button is clicked
+
+	// Image Editor configuration, using Creative CDK IPI
+	var csdkImageEditor = new Aviary.Feather({
+		apiKey: 'f1c391379dd74ed7b07fd73c2cac413d',
+		tools: ['crop','text','effects','resize'],
+		onSave: function(imageID, newURL) {
+			currentImage.src = newURL;
+			csdkImageEditor.close();
+			console.log(newURL);
+		},
+		onError: function(errorObj) {
+			console.log(errorObj.code);
+			console.log(errorObj.message);
+			console.log(errorObj.args);
+		},
+		onLoad: function() {
+      document.getElementById('edit-image-button').style.display = 'block';
+    }	    
+	});
+  
+	// Launch Image Editor
+	$('#edit-image-button').click(function() {
+		// Get the image to be edited
+		currentImage = $('#editable-image')[0];
+
+		csdkImageEditor.launch({
+			image: currentImage.id,
+			url: currentImage.src
+		});
+	});
+
+	// Reset
+	$('#reset-image-button').click(function() {
+
+		if ($('#editable-image').attr('src') === originalImageSrc) {
+			alert('Sorry, nothing to reset');
+		}
+		else {
+			$('#editable-image').attr('src', originalImageSrc);
+		}
+	});
+});
--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 # Image Editor
-hr
+<hr>
 ## How to install
-hr
-`git clone` - clone repository
-`npm install` - install all packages
-`npm run start` - create style.css and start site on localhost
+<hr>
+`git clone` - clone repository;
+`npm install` - install all packages;
+`npm run start` - start site on localhost;
